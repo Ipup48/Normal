b8b504f8766ebaf7cb2c60867fb5b77c35f8af1c
Wuttichai 022 <674259022@webmail.npru.ac.th>
2025-03-20 10:17:22 +0700

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -4,11 +4,11 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // รับค่าแม่สูตรคูณจากผู้ใช้
+       
         System.out.print("ป้อนค่าแม่สูตรคูณ (เช่น 2, 5, 9): ");
         int number = scanner.nextInt();
 
-        // แสดงแม่สูตรคูณ
+        
         System.out.println("แม่สูตรคูณของ " + number + " คือ:");
         for (int i = 1; i <= 12; i++) {
             System.out.println(number + " x " + i + " = " + (number * i));
